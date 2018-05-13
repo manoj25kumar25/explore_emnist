3e3957764a0c8a71f30d2319be25a19a02f1636d
manoj25kumar25 <39129941+manoj25kumar25@users.noreply.github.com>
2018-05-14 00:58:49 +0530

    added text border
    improve detection rate + added scope for hand written text

--- cnn_test.py
+++ cnn_test.py
@@ -12,12 +12,126 @@ import numpy as np
 import pickle
 
 text_list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ']
-
+innr_flg=0
 
 
 def show_img(img,txt):
 	cv2.imshow(txt, img)
 	cv2.waitKey()
+
+def get_innr_contour(inr_rects,im,im_th,cr):
+	print(inr_rects)
+	#[(40, 0, 635, 320)]
+	cntr_mid=int(inr_rects[2]/2)
+	cntr_width_splt=int(inr_rects[2]*0.2)
+	cntr_width_ovrlp=int(cntr_width_splt*0.2)
+	x_cntr=inr_rects[0]
+	nw_pnt=[]
+	while(x_cntr<=inr_rects[2]):
+		nw_pnt=nw_pnt+[(x_cntr,inr_rects[1],cntr_width_splt,inr_rects[3])]
+		x_cntr=x_cntr+cntr_width_splt-cntr_width_ovrlp
+	print(nw_pnt)
+	return nw_pnt
+
+def get_contours(im_th):
+	# Find contours in the image
+	im2,ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	#im2,ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_TREE   , cv2.CHAIN_APPROX_SIMPLE)
+	print(im2)
+	print(len(ctrs))
+	print(hier)
+	#exit()
+	# Get rectangles contains each contour
+	rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+	print(len(rects))
+	rects=line_segmenter(rects)
+	print(rects)
+	print(len(rects))
+	return rects
+
+def draw_contour(rects,im,im_th,cr):
+	global innr_flg
+	text=""
+	for rect in rects:
+		# Draw the rectangles
+		cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 5) 
+		
+		# Make the rectangular region around the digit
+		leng = int(rect[2] * 0.2)
+		if(leng>=rect[0]):
+			leng=rect[0]
+		pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+		pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+		roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
+		#roi = im_th[rect[1]:rect[1]+rect[3],rect[0]:rect[0]+rect[2]]
+		roi = im_th[rect[1]:rect[1]+rect[3],rect[0]-leng:rect[0]+rect[2]+leng]
+		print(roi)
+		if(len(set(roi.flatten('F')))>1):
+			# Resize the image
+			if roi.any():
+				''' 
+				#cursive inner contor need fix 
+				print("Inter contour")
+				inr_cntr_ln=len(cv2.findContours(roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE))
+				print(len(cv2.findContours(roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)))
+				if(inr_cntr_ln>1 and innr_flg==0):
+					print("\n\n####\n\n")
+					print(inr_cntr_ln)
+					innr_flg=1
+					inr_rects=get_innr_contour(rect,im,im_th,cr)
+					draw_contour(inr_rects,im,im_th,cr)
+					#get_innr_contour(inr_rects,im,im_th,cr)
+				'''
+				BLACK = [0,0,0]
+				roi=cv2.copyMakeBorder(roi,10,10,10,10,cv2.BORDER_CONSTANT,value=BLACK)
+				#show_img(roi,"after placing rectangles")
+				roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+				roi = cv2.dilate(roi, (3, 3))
+				
+				
+				print (type(roi))
+				#cv2.imshow("Resulting Image with Rectangular ROIs", roi)
+				#cv2.waitKey()
+				# Calculate the HOG features
+				roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+				print("hog feature shape ")
+				print(roi_hog_fd.shape)
+				print("vs")
+				print(roi.shape)
+				roi=roi.flatten('F')
+				#print(roi)
+				#img_conv2d_arr,mapping=preprocess.load_sngl_img(np.array([roi_hog_fd],'float64'))
+				img_conv2d_arr,mapping=preprocess.load_sngl_img(np.array([roi]),'data')
+				#nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
+				
+				print(np.array([roi_hog_fd],'float64').shape)
+				
+				nbr =  cr.read_text(img_conv2d_arr, mapping)
+				preds=nbr
+				print(str(nbr))
+				vlu=str(nbr)
+				vlu=text_list[int(nbr) -1]
+				text=text+vlu
+				print(vlu+" -- "+str(int(nbr[0])))
+				cv2.putText(im, vlu, (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 3, (0, 250, 224), 5)
+			else:
+				print("\n\n\n NOt detected \n\n\n\n")
+				print(roi)
+				print(rect)
+				print(leng)
+				print(pt1) 
+				print(pt2) 
+		else:
+			print("Flatten have one bit only ")
+			#print(im.shape)
+			#print(rect)
+			#print("vs")
+			#print(str(rect[1])+":"+str(rect[1])+"+"+str(rect[3])+","+str(rect[0])+"-"+str(leng)+":"+str(rect[0])+"+"+str(rect[2])+"+"+str(leng))
+			#print(set(roi.flatten('F')))
+				
+	print(text)
+	return text,im
+
 def line_segmenter(arr):
 	print("line segmenter")
 	arr.sort(key=lambda i: (i[1]))
@@ -80,9 +194,9 @@ def main(train):
 	#im = cv2.imread("C:/Users/manoj/Documents/py_workspace/ocr/example4.png")
 	#im = cv2.imread("C:/Users/manoj/Documents/py_workspace/ocr/example2.jpg")
 	#im = cv2.imread("C:/Users/manoj/Documents/py_workspace/ocr/example6.jpg")
-	im = cv2.imread("C:/Users/manoj/Documents/py_workspace/ocr/example7.jpg")
+	#im = cv2.imread("C:/Users/manoj/Documents/py_workspace/ocr/example7.jpg")
 	#im = cv2.imread("C:/Users/manoj/Documents/py_workspace/ocr/example5.png")
-	#im = cv2.imread("C:/Users/manoj/Documents/py_workspace/ocr/example3.png")
+	im = cv2.imread("C:/Users/manoj/Documents/py_workspace/ocr/example3.png")
 	
 	height, width = im.shape[:2]
 	multiply=5
@@ -98,10 +212,10 @@ def main(train):
 	ret, im_th = cv2.threshold(im_gray, 160, 255, cv2.THRESH_BINARY_INV)
 	#ret, im_th = cv2.threshold(im_gray, 160, 255, cv2.THRESH_BINARY_INV)
 	show_img(im_th," after threshold filter")
-	
+	'''
 	# Find contours in the image
 	im2,ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	#im2,ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_LIST   , cv2.CHAIN_APPROX_SIMPLE)
+	#im2,ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_TREE   , cv2.CHAIN_APPROX_SIMPLE)
 	print(im2)
 	print(len(ctrs))
 	print(hier)
@@ -116,23 +230,31 @@ def main(train):
 	# For each rectangular region, calculate HOG features and predict
 	# the digit using Linear SVM.
 	print("Detected : ")
+	'''
+	rects=get_contours(im_th)
 	text=""
+	'''
 	for rect in rects:
 		# Draw the rectangles
-		cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 1) 
+		cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 5) 
 		
 		# Make the rectangular region around the digit
 		leng = int(rect[2] * 0.2)
+		if(leng>=rect[0]):
+			leng=rect[0]
 		pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
 		pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
 		roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
 		#roi = im_th[rect[1]:rect[1]+rect[3],rect[0]:rect[0]+rect[2]]
 		roi = im_th[rect[1]:rect[1]+rect[3],rect[0]-leng:rect[0]+rect[2]+leng]
 		print(roi)
-		show_img(roi,"after placing rectangles")
 		if(len(set(roi.flatten('F')))>1):
 			# Resize the image
 			if roi.any():
+				
+				print("Inter contour")
+				print(len(cv2.findContours(roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)))
+				show_img(roi,"after placing rectangles")
 				roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
 				roi = cv2.dilate(roi, (3, 3))
 				print (type(roi))
@@ -159,7 +281,7 @@ def main(train):
 				vlu=text_list[int(nbr) -1]
 				text=text+vlu
 				print(vlu+" -- "+str(int(nbr[0])))
-				cv2.putText(im, vlu, (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 250, 224), 3)
+				cv2.putText(im, vlu, (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 3, (0, 250, 224), 5)
 			else:
 				print("\n\n\n NOt detected \n\n\n\n")
 				print(roi)
@@ -167,9 +289,17 @@ def main(train):
 				print(leng)
 				print(pt1) 
 				print(pt2) 
-				
+		else:
+			print("Flatten have one bit only ")
+			#print(im.shape)
+			#print(rect)
+			#print("vs")
+			#print(str(rect[1])+":"+str(rect[1])+"+"+str(rect[3])+","+str(rect[0])+"-"+str(leng)+":"+str(rect[0])+"+"+str(rect[2])+"+"+str(leng))
+			#print(set(roi.flatten('F')))
 				
 	print(text)
+'''
+	text,im=draw_contour(rects,im,im_th,cr)
 	cv2.imshow("Resulting Image with Rectangular ROIs", im)
 	cv2.waitKey()
 
